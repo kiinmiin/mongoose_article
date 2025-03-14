8a4f7b8b2e6cf0cd59a1ec7184bbb3c5e676f6dd
kiinmiin <rasmo.kiin@voco.ee>
2025-03-14 15:30:06 +0200

    Set up project and mongodb made a model for an article and made the first controller

---
+++ index.ts
@@ -0,0 +1,28 @@
+import express, { Express, Request, Response } from "express";
+import mongoose from "mongoose";
+import articleController from "./controllers/articleController";
+
+mongoose.connect("mongodb+srv://rasmo:yiZ6zOZHd6J65GG2@cluster0.p92qo.mongodb.net/test");
+const database = mongoose.connection;
+
+database.on('error', (error) => {
+    console.log(error);
+})
+
+database.once('connected', () => {
+    console.log('Connected to MongoDB');
+})
+
+const app: Express = express();
+
+app.use(express.json());
+
+app.get('/', (req: Request, res: Response) => {
+  res.send('Express + TypeScript Server');
+});
+
+app.use('/', articleController);
+
+app.listen(3010,() => {
+  console.log(`[server]: Server is running at http://localhost:3010`);
+});
